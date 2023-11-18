3117d18d055c23e1fff3cf3432287f0ab6b0b74f
Juan Riccetto <juanmartinre@gmail.com>
2023-11-17 23:59:33 -0300

    axios typo fix

---
+++ src/services/userServices.js
@@ -0,0 +1,51 @@
+import { api } from "../utils/api";
+
+export const getUsers = async () => {
+  try {
+    const users = await api.get("/users");
+    return users.data;
+  } catch (error) {
+    console.error("Error al obtener los usuarios:", error);
+    throw error;
+  }
+};
+
+export const getUserTable = async () => {
+  try {
+    const users = await api.get("/user-table");
+    return users.data;
+  } catch (error) {
+    console.error("Error al obtener los usuarios:", error);
+    throw error;
+  }
+};
+
+export const createUser = async (body) => {
+  try {
+    await api.post("/users", body);
+  } catch (error) {
+    console.error("Error al obtener los usuarios:", error);
+    throw error;
+  }
+};
+
+export const postUser = async (body) => {
+  try {
+    await api.post("http://localhost:8080/user", body);
+  } catch (error) {
+    console.error("Error al obtener los usuarios:", error);
+    throw error;
+  }
+};
+
+export const sendMail = async (destinatario) => {
+  const body = {
+    destinatario,
+  };
+  try {
+    await api.post("/mail", body);
+  } catch (error) {
+    console.error("Error al enviar mail:", error);
+    throw error;
+  }
+};
